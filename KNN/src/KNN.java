6f06aeab4cc95b0316e669c8c48f4ca30d4c472e
Miao Fan <fanmiao.cslt.thu@gmail.com>
2013-08-03 19:12:21 +0800

    KNN Version 1.0

---
+++ KNN/src/KNN.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: michael_fan
+ * Date: 13-8-3
+ * Time: 下午7:07
+ * To change this template use File | Settings | File Templates.
+ */
+public class KNN {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
